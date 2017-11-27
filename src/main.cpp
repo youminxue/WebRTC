b1672fc3e485adf2959dfc4f11b3ed7e52171217
armite <armite.wzm@gmail.com>
2017-11-27 16:45:41 +0800

    sdl2 window can show

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include <syscall.h>
 #include <unistd.h>
 
+#include "SDL.h"
+
 int main()
 {
   std::cout << "tid:" << syscall(SYS_gettid) << std::endl;
@@ -19,6 +21,31 @@ int main()
   rtcSignal.set_message_listener([&](message::ptr const& msg){
   });
 
-  while(1);
+  SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);
+
+  SDL_Window *window = SDL_CreateWindow("WebRTC", 10, 10, 640, 480, SDL_WINDOW_OPENGL);
+  SDL_Surface *surface = SDL_GetWindowSurface(window);
+  SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 255, 0, 0));
+  SDL_UpdateWindowSurface(window);
+
+  SDL_Event event;
+  bool runing = true;
+  while(runing){
+    SDL_WaitEvent(&event);
+    switch(event.type) {
+      case SDL_KEYDOWN:
+        printf("keyboard:%s\n", SDL_GetKeyName(event.key.keysym.sym));
+        break;
+      case SDL_QUIT:
+        runing = false;
+        break;
+      default:
+        //printf("event %x\n", event.type);
+        break;
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
 }
